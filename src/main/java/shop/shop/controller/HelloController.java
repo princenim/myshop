c55a25b1ba1d7bd7df7841c01f8680a20192bcab
hazel <waithjno@gmail.com>
2023-04-05 14:02:05 +0900

    feat : common response

--- src/main/java/shop/shop/controller/HelloController.java
+++ src/main/java/shop/shop/controller/HelloController.java
@@ -1,13 +1,14 @@
 package shop.shop.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import shop.shop.model.JsonResult;
 
 /**
  * @author hazel
  */
-@Controller
+@RestController
 public class HelloController {
 
     @GetMapping("hello")
@@ -15,6 +16,11 @@ public class HelloController {
         model.addAttribute("data", "heyyyy");
         //tempaltes에 hello.html로 연결
         return "hello";
+    }
+
+    @GetMapping("test")
+    public JsonResult test(){
+        return JsonResult.ok("json test");
     }
 
 }
