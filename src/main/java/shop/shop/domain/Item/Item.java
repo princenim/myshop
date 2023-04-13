29ef893302512004fc5ef5c970c8f6edde1c6cf9
hazel <waithjno@gmail.com>
2023-04-14 01:06:37 +0900

    feat :  modify Item entity class

--- src/main/java/shop/shop/domain/Item/Item.java
+++ src/main/java/shop/shop/domain/Item/Item.java
@@ -4,6 +4,7 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 import shop.shop.domain.Category;
+import shop.shop.exception.NotEnoughStockException;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -38,6 +39,24 @@ public abstract class Item {
     //객체는 다대다가 가능하지만, 관계형 디비는 1대다 , 다대1의 중간 테이블이 필요
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<>();
+
+    //비즈니스 로직- 1. 재고를 늘리고, 2. 재고를 줄이기
+    //객체지향적-  setter을 사용하는게 아니고, 데이터를 가지고 있는 쪽에 entity에 로직을 추가하는게 좋음.
+    public void addStock(int quantity){
+        this.stockQuantity +=  quantity;
+    }
+
+    public  void removeStock(int quantity){
+        int restStock= this.stockQuantity = quantity;
+        if(restStock <0 ){
+            throw new NotEnoughStockException("need more stock");
+        }
+
+        this.stockQuantity = restStock;
+
+
+    }
+
 
 
 
---
+++ src/main/java/shop/shop/exception/NotEnoughStockException.java
@@ -0,0 +1,29 @@
+package shop.shop.exception;
+
+/**
+ * @author hazel
+ */
+public class NotEnoughStockException extends RuntimeException{
+
+
+    //오버라이드
+    public NotEnoughStockException() {
+        super();
+    }
+
+    public NotEnoughStockException(String message) {
+        super(message);
+    }
+
+    public NotEnoughStockException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public NotEnoughStockException(Throwable cause) {
+        super(cause);
+    }
+
+    protected NotEnoughStockException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+}
