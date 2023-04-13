087c913df21622fe69d800f501c6a67ea9c6d36a
hazel <waithjno@gmail.com>
2023-04-14 01:12:52 +0900

    feat :  Item repository

--- src/main/java/shop/shop/domain/Item/Item.java
+++ src/main/java/shop/shop/domain/Item/Item.java
@@ -34,7 +34,6 @@ public abstract class Item {
     private int stockQuantity;
 
 
-
     //중간 테이블 매핑이 필요함
     //객체는 다대다가 가능하지만, 관계형 디비는 1대다 , 다대1의 중간 테이블이 필요
     @ManyToMany(mappedBy = "items")
@@ -42,13 +41,13 @@ public abstract class Item {
 
     //비즈니스 로직- 1. 재고를 늘리고, 2. 재고를 줄이기
     //객체지향적-  setter을 사용하는게 아니고, 데이터를 가지고 있는 쪽에 entity에 로직을 추가하는게 좋음.
-    public void addStock(int quantity){
-        this.stockQuantity +=  quantity;
+    public void addStock(int quantity) {
+        this.stockQuantity += quantity;
     }
 
-    public  void removeStock(int quantity){
-        int restStock= this.stockQuantity = quantity;
-        if(restStock <0 ){
+    public void removeStock(int quantity) {
+        int restStock = this.stockQuantity = quantity;
+        if (restStock < 0) {
             throw new NotEnoughStockException("need more stock");
         }
 
@@ -56,8 +55,6 @@ public abstract class Item {
 
 
     }
-
-
 
 
 }
---
+++ src/main/java/shop/shop/repository/ItemRepository.java
@@ -0,0 +1,41 @@
+package shop.shop.repository;
+
+import jakarta.persistence.EntityManager;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+import shop.shop.domain.Item.Item;
+
+import java.util.List;
+
+/**
+ * @author hazel
+ */
+@Repository
+@RequiredArgsConstructor
+public class ItemRepository {
+
+    private final EntityManager em;
+
+    public void save(Item item){
+        if(item.getId() == null){
+            //아이템 저장 - 처음에 아이템은 jpa를 저장하기 전까지  id가 없음 -> 새로 생성한 객체라는 뜻
+            em.persist(item);
+        }else {
+            //이미 db에 있음.
+            em.merge(item);
+        }
+    }
+
+    public Item findOne(Long id){
+        return  em.find(Item.class, id);
+    }
+
+    private List<Item> findAll(){
+        return em.createQuery("select i from Item i", Item.class).getResultList();
+    }
+
+
+
+
+
+}
